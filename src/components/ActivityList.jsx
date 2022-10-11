cee20b94b2f06a0a46abb0abad9e52815a5ea97d
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-10-11 10:19:05 +0700

    add activity card list

--- src/components/ActivityList.jsx
+++ src/components/ActivityList.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 
-function ActivityList() {
+function ActivityList({ title, date }) {
+
+    function dateFunc(value) {
+        let val = value.slice(0, 10).split("-");
+        let monthNumber = parseInt(val[1]);
+        let monthName = [
+            "Januari",
+            "Februari",
+            "Maret",
+            "April",
+            "Mei",
+            "Juni",
+            "Juli",
+            "Agustus",
+            "September",
+            "Oktober",
+            "November",
+            "Desember",
+        ][monthNumber - 1];
+
+        val[1] = monthName;
+
+        val = val.reverse().join(" ");
+
+        return val;
+    }
     return (
-        <>
-            <div className='min-h-[70vh] lg:min-h-[60vh] flex items-center'>
+        <div className='p-6 w-full lg:w-56 h-56 bg-white rounded-xl border border-gray-200 shadow-xl inline-flex flex-col justify-between'>
+            <h1 className='mb-2 text-xl font-bold' data-cy='activity-item-title'>
+                {title}
+            </h1>
+            <div className='flex flex-row justify-between items-center'>
+                <p className=' lg:font-medium text-gray-700' data-cy="activity-item-date">
+                    {dateFunc(date)}
+                </p>
+                <button className='btn btn-circle btn-sm btn-outline btn-error' data-cy="activity-item-delete-button">
+                    <i class='bx bx-trash'></i>
+                </button>
 
             </div>
-        </>
+
+        </div>
     )
 }
 
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -3,6 +3,7 @@ import EmptyState from '../components/EmptyState'
 import TitleBar from '../components/TitleBar'
 import { Link } from 'react-router-dom'
 import axios from 'axios';
+import ActivityList from '../components/ActivityList';
 
 function Dashboard() {
 
@@ -23,15 +24,18 @@ function Dashboard() {
     return (
         <>
             <TitleBar />
-            <div className='min-h-[70vh] lg:min-h-[60vh] flex items-center'>
-                <EmptyState data-cy="activity-empty-state" main />
-            </div>
-            {activity.map((act) => (
-                <div key={act.id}>
-                    <p> {act.title} </p>
-
+            {activity.length == 0 ?
+                <div className='min-h-[70vh] lg:min-h-[60vh] flex items-center'>
+                    <EmptyState data-cy="activity-empty-state" main />
                 </div>
-            ))}
+                :
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-7 lg:mt-13' data-cy="activity-item">
+                    {activity.map((act) => (
+                        <ActivityList key={act.id} title={act.title} date={act.created_at} />
+                    ))}
+                </div>
+
+            }
         </>
     )
 }
